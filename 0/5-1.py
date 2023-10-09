8ec2e2de44c796b8cc684c44d0f764ebf44ffa3a
zzzzzuuuuu <agu0830@naver.com>
2023-10-09 16:47:27 +0900

    feat: 구매한 옷의 가격 price가 주어질 때, 지불해야 할 금액 구하기

---
+++ 0/5-1.py
@@ -0,0 +1,15 @@
+def solution(price):
+    if price >= 500000:
+        return int(price*0.8)
+    elif price >= 300000:
+        return int(price*0.9)
+    elif price >= 100000:
+        return int(price*0.95)
+    else: return price
+
+    # 또 다른 풀이
+def solution(price):
+    discount_rates = {500000: 0.8, 300000: 0.9, 100000: 0.95, 0: 1}
+    for discount_price, discount_rate in discount_rates.items():
+        if price >= discount_price:
+            return int(price * discount_rate)

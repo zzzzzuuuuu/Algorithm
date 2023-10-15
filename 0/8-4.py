558684798a6c16e63cac887118bc48e66ae0778a
zzzzzuuuuu <agu0830@naver.com>
2023-10-15 21:33:55 +0900

    feat: 자연수 n이 매개변수로 주어질 때 두 숫자의 곱이 n인 자연수 순서쌍의 개수 return

--- 0/8-4.py
+++ 0/8-4.py
@@ -1,7 +1,19 @@
+# def solution(n):
+#     result = 0
+#     for a in range(n+1):
+#         for b in range(a+1):
+#             if a >= b and a*b == n:
+#                 result += 1
+#     return result*2 if (n ** .5) != int(n ** .5) else result*2-1 # 시간초과 뜸
+
+# 정답
 def solution(n):
     result = 0
-    for a in range(n+1):
-        for b in range(a+1):
-            if a >= b and a*b == n:
-                result += 1
-    return result*2 if (n ** .5) != int(n ** .5) else result*2-1 # 시간초과 뜸
+    for i in range(1, n+1):
+        if n % i == 0:
+            result += 1
+    return result
+
+# 또 다른 풀이
+def solution2(n):
+    return len([number for number in range(1, n+1) if n % number == 0])

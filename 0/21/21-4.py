45a67316d24c07327f02341cfdf36031f931286a
zzzzzuuuuu <agu0830@naver.com>
2024-03-10 20:31:58 +0900

    폴더구조 변경

---
+++ 0/21/21-4.py
@@ -0,0 +1,19 @@
+def solution(spell, dic):
+    count = 0
+    for c in dic:
+        count = 0
+        for s in spell:
+            if s in c:
+                count += 1
+        if count == len(spell):
+            return 1
+    return 2
+
+
+# 또 다른 풀이
+def solution2(spell, dic):
+    spell = set(spell)
+    for s in dic:
+        if not spell - set(s):
+            return 1
+    return 2

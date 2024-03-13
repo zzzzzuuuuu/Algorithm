266dea33dd39c080c876b8e84350b914e818a88e
zzzzzuuuuu <agu0830@naver.com>
2024-03-13 17:04:04 +0900

    세 선분의 시작과 끝 좌표가 들어있는 2차원 배열 lines가 매개변수로 주어질 때, 두 개 이상의 선분이 겹치는 부분의 길이 구하기

---
+++ 0/22/22-3.py
@@ -0,0 +1,6 @@
+def solution(lines):
+    s1 = set(i for i in range(lines[0][0], lines[0][1]))
+    s2 = set(i for i in range(lines[1][0], lines[1][1]))
+    s3 = set(i for i in range(lines[2][0], lines[2][1]))
+
+    return len((s1 & s2) | (s2 & s3) | (s3 & s1))

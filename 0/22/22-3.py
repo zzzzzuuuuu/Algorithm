fda14880c09f63a157f603c605e935feb68063b7
zzzzzuuuuu <agu0830@naver.com>
2024-03-14 17:48:01 +0900

    겹치는 선분의 길이 또 다른 풀이 추가

--- 0/22/22-3.py
+++ 0/22/22-3.py
@@ -4,3 +4,8 @@ def solution(lines):
     s3 = set(i for i in range(lines[2][0], lines[2][1]))
 
     return len((s1 & s2) | (s2 & s3) | (s3 & s1))
+
+# 또 다른 풀이
+def solution2(lines):
+    sets = [set(range(min(line), max(line))) for line in lines]
+    return len(sets[0] & sets[1] | sets[0] & sets[2] | sets[1] & sets[2])

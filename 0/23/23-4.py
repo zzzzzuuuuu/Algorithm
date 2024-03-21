bbcf2d1153c557b8198593b1bdcb3c233d143d69
zzzzzuuuuu <agu0830@naver.com>
2024-03-21 22:04:28 +0900

    로그인 성공 또다른 풀이 추가

--- 0/23/23-4.py
+++ 0/23/23-4.py
@@ -3,3 +3,10 @@ def solution(id_pw, db):
         if id_pw[0] in i:
             return "login" if id_pw[1] in i else "wrong pw"
     return "fail"
+
+
+# 또 다른 풀이
+def solution2(id_pw, db):
+    if db_pw := dict(db).get(id_pw[0]): # db를 사전으로 바꾼 뒤 해당 아이디(키)의 비밀번호(밸류)를 db_pw라는 변수에 할당, 없으면 none
+        return "login" if db_pw == id_pw[1] else "wrong pw"
+    return "fail"

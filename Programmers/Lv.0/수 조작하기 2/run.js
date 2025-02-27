bc9aae455427c03d29b8bcc3e28efd885d25bfa1
zzzzzuuuuu <agu0830@naver.com>
2025-02-27 14:51:19 +0900

    [Lv.0] 수 조작하기 2

---
+++ Programmers/Lv.0/수 조작하기 2/run.js
@@ -0,0 +1,15 @@
+function solution(numLog) {
+  let result = '';
+
+  for (let i = 0; i < numLog.length; i++) {
+    const diff = numLog[i+1] - numLog[i];
+    switch(diff) {
+      case 1: result += 'w'; break;
+      case -1: result += 's'; break;
+      case 10: result += 'd'; break;
+      case -10: result += 'a'; break;
+    }
+  }
+
+  return result;
+}

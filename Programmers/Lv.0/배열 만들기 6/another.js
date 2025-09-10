51606f5afc9ef924e4dc2b3cf40c814ceb0f09eb
zzzzzuuuuu <agu0830@naver.com>
2025-09-10 16:05:11 +0900

    [Lv.0] 배열 만들기

---
+++ Programmers/Lv.0/배열 만들기 6/another.js
@@ -0,0 +1,12 @@
+function solution(arr) {
+  let stk = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (stk[stk.length - 1] === arr[i]) {
+      stk.pop();
+    } else {
+      stk.push(arr[i]);
+    }
+  }
+  return stk.length === 0 ? [-1] : stk;
+}

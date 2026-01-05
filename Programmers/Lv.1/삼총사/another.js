45e8c8b8b1d23b134e7cb9b9cf8027e61982c2cf
zzzzzuuuuu <agu0830@naver.com>
2026-01-05 13:12:43 +0900

    [Lv.1] 삼총사

---
+++ Programmers/Lv.1/삼총사/another.js
@@ -0,0 +1,15 @@
+function solution(number) {
+  let count = 0;
+
+  const combination = (current, start) => {
+    if (current.length === 3) {
+      count += number.reduce((acc, cur) => acc + cur, 0) === 0 ? 1 : 0;
+      return;
+    }
+    for (let i = start; i < number.length; i++) {
+      combination([...current, number[i]], i+1);
+    }
+  }
+  combination([], 0);
+  return count;
+}

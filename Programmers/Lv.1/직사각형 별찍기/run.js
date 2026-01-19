f8fab11bef2d2f21eadfd123a500fe3d94977a35
zzzzzuuuuu <agu0830@naver.com>
2026-01-19 14:24:56 +0900

    [Lv.1] 직사각형 별찍기

---
+++ Programmers/Lv.1/직사각형 별찍기/run.js
@@ -0,0 +1,8 @@
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', data => {
+  const n = data.split(" ");
+  const a = Number(n[0]), b = Number(n[1]);
+
+  const line = '*'.repeat(a);
+  console.log((line + '\n').repeat(b));
+});

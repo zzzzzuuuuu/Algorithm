c62a4f94895de2d74c39a5b0fb4f3dd9e0a05fd7
zzzzzuuuuu <agu0830@naver.com>
2026-02-19 13:17:53 +0900

    [Lv.1] 소수 찾기

---
+++ Programmers/Lv.1/소수 찾기/run.js
@@ -0,0 +1,14 @@
+function solution(n) {
+  const isPrime = Array(n + 1).fill(true);
+  isPrime[0] = isPrime[1] = false;
+
+  for (let i = 2; i * i <= n; i++) {
+    if (isPrime[i]) {
+      for (let j = i * i; j <= n; j += i) {
+        isPrime[j] = false;
+      }
+    }
+  }
+
+  return isPrime.filter(Boolean).length;
+}
